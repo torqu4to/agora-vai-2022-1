75f0d2fa4a465fbaf9839831a3ea7376392c352d
Fausto G. Cintra <professor@faustocintra.com.br>
2022-05-23 09:21:08 -0300

    (23/05) Início do componente de formulário (intervalo)

--- src/routed/AlunoForm.js
+++ src/routed/AlunoForm.js
@@ -1,7 +1,168 @@
 import React from 'react'
+import TextField from '@mui/material/TextField'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { LocalizationProvider, DesktopDatePicker } from '@mui/x-date-pickers'
+import ptLocale from 'date-fns/locale/pt-BR'
+import { makeStyles } from '@mui/styles'
+import InputMask from 'react-input-mask'
+import MenuItem from '@mui/material/MenuItem'
+
+const useStyles = makeStyles(theme => ({
+    form: {
+        maxWidth: '90%',
+        margin: '0 auto',
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        '& .MuiFormControl-root': {
+            minWidth: '200px',
+            maxWidth: '500px',
+            marginBottom: '24px'
+        }
+    }
+}))
+
+const unidadesFed = [
+    { sigla: 'DF', nome: 'Distrito Federal' },
+    { sigla: 'ES', nome: 'Espírito Santo' },
+    { sigla: 'GO', nome: 'Goiás' },
+    { sigla: 'MS', nome: 'Mato Grosso do Sul' },
+    { sigla: 'MG', nome: 'Minas Gerais' },
+    { sigla: 'PR', nome: 'Paraná' },
+    { sigla: 'RJ', nome: 'Rio de Janeiro' },
+    { sigla: 'SP', nome: 'São Paulo' }
+]
 
 export default function AlunoForm() {
+
+    const classes = useStyles()
+
+    const [state, setState] = React.useState(
+        // Lazy initalizer
+        () => ({
+            aluno: {}
+        })
+    )
+    const {
+        aluno
+    } = state 
+
     return (
-        <h1>Cadastro de alunos</h1>
+        <>
+            <h1>Cadastro de alunos</h1>
+
+            <form className={classes.form}>
+                
+                <TextField 
+                    id="nome" 
+                    label="Nome completo"
+                    value={aluno.nome}
+                    variant="filled"
+                    placeholder="Informe o nome completo do(a) aluno(a)"
+                    required
+                    fullWidth
+                />
+
+                <LocalizationProvider dateAdapter={AdapterDateFns} locale={ptLocale}>
+                    <DesktopDatePicker
+                        label="Data de nascimento"
+                        inputFormat="dd/MM/yyyy"
+                        value={aluno.data_nascimento}
+                        renderInput={(params) => <TextField
+                            id="data_nascimento"
+                            variant="filled"
+                            required
+                            fullWidth 
+                            {...params} 
+                        />}
+                    />
+                </LocalizationProvider>
+
+                <TextField 
+                    id="doc_identidade" 
+                    label="Documento de identidade"
+                    value={aluno.doc_identidade}
+                    variant="filled"
+                    placeholder="Informe o documento de identidade"
+                    required
+                    fullWidth
+                />
+
+                <InputMask
+                    mask="999.999.999-99"
+                    value={aluno.cpf}
+                >
+                    {
+                        () => <TextField 
+                            id="cpf" 
+                            label="CPF"
+                            variant="filled"
+                            placeholder="Informe o CPF"
+                            required
+                            fullWidth
+                        />
+                    }
+                </InputMask>
+
+                <TextField 
+                    id="logradouro" 
+                    label="Logradouro (Rua, Av., etc.)"
+                    value={aluno.logradouro}
+                    variant="filled"
+                    placeholder="Informe o logradouro"
+                    required
+                    fullWidth
+                />
+
+                <TextField 
+                    id="num_imovel" 
+                    label="Nº"
+                    value={aluno.num_imovel}
+                    variant="filled"
+                    placeholder="Informe o número do imóvel"
+                    required
+                    fullWidth
+                />
+
+                <TextField 
+                    id="complemento" 
+                    label="Complemento"
+                    value={aluno.complemento}
+                    variant="filled"
+                    placeholder="Informe o complemento do imóvel (se houver)"
+                    fullWidth
+                />
+
+                <TextField 
+                    id="municipio" 
+                    label="Município"
+                    value={aluno.municipio}
+                    variant="filled"
+                    placeholder="Informe o município"
+                    required
+                    fullWidth
+                />
+
+                <TextField 
+                    id="uf" 
+                    label="UF"
+                    value={aluno.uf}
+                    variant="filled"
+                    placeholder="Informe a UF"
+                    required
+                    fullWidth
+                    select
+                >
+                    {
+                        unidadesFed.map(uf => (
+                            <MenuItem key={uf.sigla} value={uf.sigla}>
+                                {uf.nome}
+                            </MenuItem>
+                        ))
+                    }
+                </TextField>
+
+            </form>
+        </>
     )
 }
